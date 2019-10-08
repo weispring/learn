042245664b65620003241b046c0c67f53b5a824e
li.xianchun <li.xianchun@vpclub.cn>
2019-10-08 17:14:25 +0800

    classLoad

---
+++ learn-jdk/src/main/java/com.lxc.learn.jdk/clasload/FileSystemClassLoader.java
@@ -0,0 +1,64 @@
+package com.lxc.learn.jdk.clasload;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @author lixianchun
+ * @Description
+ * @date 2019/10/8 12:22
+ */
+@Slf4j
+public class FileSystemClassLoader extends ClassLoader{
+
+    private String rootDir;
+
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        Class classNum = new FileSystemClassLoader("E:\\lcode\\learn\\learn-jdk\\target\\com").findClass("NumTest");
+        Object object = classNum.newInstance();
+        classNum.getMethod("testByte").invoke(object, null);
+    }
+
+    public FileSystemClassLoader(String rootDir){
+        this.rootDir = rootDir;
+    }
+
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] classData = getClassData(name);
+        if (classData == null){
+            throw new ClassNotFoundException();
+        }
+        else {
+            //name 类全名
+            return defineClass(name, classData, 0, classData.length);
+        }
+    }
+
+    private byte[] getClassData(String className) {
+        String path = classNameToPath(className);
+        try {
+            InputStream ins = new FileInputStream(path);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int bytesNumRead = 0;
+            while ((bytesNumRead = ins.read(buffer)) != -1){
+                baos.write(buffer, 0, bytesNumRead);
+            }
+            return baos.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String classNameToPath(String className) {
+        return rootDir + File.separatorChar + className.replace('.', File.separatorChar) + ".class";
+    }
+
+}
--- learn-jdk/src/main/java/com.lxc.learn.jdk/clasload/Load.java
+++ learn-jdk/src/main/java/com.lxc.learn.jdk/clasload/Load.java
@@ -1,7 +1,10 @@
 package com.lxc.learn.jdk.clasload;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import sun.misc.Launcher;
 
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +18,48 @@ public class Load {
 
     public static void main(String[] args) {
 
-        ClassLoader classLoader = null;
+        new Load().loadClass();
     }
+
+    private void loadClass(){
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        try {
+            classLoader.loadClass("com.lxc.learn.jdk.basedatatype.AddAddReduceReduce");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        log.info("{}", 1);
+    }
+
+
+    /**
+     * BootStrap ClassLoader：称为启动类加载器，是Java类加载层次中最顶层的类加载器，负责加载JDK中的核心类库，
+     * 如：rt.jar、resources.jar、charsets.jar等，可通过如下程序获得该类加载器从哪些地方加载了相关的jar或class文件
+     */
+    @Test
+    public void testBootStrapClassLoader(){
+        URL[] urls = sun.misc.Launcher.getBootstrapClassPath().getURLs();
+        for (int i = 0; i < urls.length; i++) {
+            System.out.println(urls[i].toExternalForm());
+        }
+        System.out.println(System.getProperty("sun.boot.class.path"));
+    }
+
+
+    /**
+     * Launcher ExtClassLoader AppClassLoader
+     */
+    @Test
+    public void testClassLoadTree(){
+        ClassLoader loader = Load.class.getClassLoader();
+        while (loader!=null){
+            System.out.println(loader.toString());
+            loader = loader.getParent();
+        }
+        System.out.println(loader);
+    }
+
+
+
+
 }
