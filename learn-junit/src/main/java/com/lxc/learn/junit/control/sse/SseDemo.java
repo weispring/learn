c104dcc7a194ab6abd30f0472de7cc5d150e0d23
li xian chun <2629469408@qq.com>
2020-04-06 21:29:26 +0800

    sse

--- learn-junit/src/main/java/com/lxc/learn/junit/control/sse/SseDemo.java
+++ learn-junit/src/main/java/com/lxc/learn/junit/control/sse/SseDemo.java
@@ -10,6 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author lixianchun
  * @description
@@ -20,23 +25,25 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RequestMapping("/sse")
 public class SseDemo {
 
-    ThreadPoolTaskExecutor mvcTaskExecutor = new ThreadPoolTaskExecutor();
-
     @GetMapping("/sseDemo")
-    public SseEmitter sseDemo() throws InterruptedException {
+    public SseEmitter sseDemo() throws InterruptedException, IOException {
         final SseEmitter emitter = new SseEmitter(0L); //timeout设置为0表示不超时
-        mvcTaskExecutor.execute(() -> {
+        ThreadPoolExecutor taskExecutor = new ThreadPoolExecutor(1,5,60,TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(1000));
+        taskExecutor.execute(() -> {
             try {
-                for(int i=0;i<100;i++){
-                    emitter.send("hello"+i);
-                    log.info("emit:{}","hello"+i);
-                    Thread.sleep(1000*1);
-                }
-                emitter.complete();
+
             } catch (Exception e) {
                 emitter.completeWithError(e);
             }
         });
+        for(int i=0;i<100;i++){
+            emitter.send("hello"+i);
+            log.info("emit:{}","hello"+i);
+            Thread.sleep(1000*1);
+        }
+        emitter.complete();
+
         log.error("已完成");
         return emitter;
     }
