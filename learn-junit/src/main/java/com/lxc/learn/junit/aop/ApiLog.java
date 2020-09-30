8b14a25ea9a5ddceee2174c8c07cd86435313a77
li.xianchun <li.xianchun@vpclub.cn>
2020-09-30 17:36:35 +0800

    annotation not find

--- learn-junit/src/main/java/com/lxc/learn/junit/aop/ApiLog.java
+++ learn-junit/src/main/java/com/lxc/learn/junit/aop/ApiLog.java
@@ -2,6 +2,9 @@ package com.lxc.learn.junit.aop;
 
 import java.lang.annotation.*;
 
+/**
+ * 不加元注解的话，org.aopalliance.intercept.MethodInterceptor的实现中，无法获取method 上的注解信息
+ */
 @Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
